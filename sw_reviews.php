e7355e057254199cfbe4073d30ec71e06e5f8fc6
Sunil Williams <sunil@sunil.co.nz>
2014-03-21 14:32:51 +1300

    initial commit

---
+++ sw_reviews.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:  sw_reviews
+ * Plugin URI:   sunil@sunil.co.nz
+ * Description:  performance reviews
+ * Version: 0.1
+ * Author: Sunil Williams
+ * Author URI: http://sunil.co.nz
+ *
+ * License:  GPLv2
+ */
+?>
